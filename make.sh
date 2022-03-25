bd270e6a7ea17b5b6ef129462f909a159b06d11d
Julien Chemillier <chemillier.julien@gmail.com>
2022-03-25 19:06:36 +0100

    Move files

--- make.sh
+++ make.sh
@@ -4,7 +4,7 @@ if [[ $1 == "preview" ]]; then
 	[[ $2 ]] || set -- "$1" "build"
 	if [[ $2 == "build" ]]; then
 		mkdir -p out
-		gcc src/preview_mnist.c -o out/preview_mnist
+		gcc src/mnist/preview.c -o out/preview_mnist
 		exit
 	elif [[ $2 == "train" ]]; then
 		[[ -f out/preview_mnist ]] || $0 preview build
