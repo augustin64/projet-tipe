0fc627182833f33029c9987ef223b2049be1f3f1
Julien Chemillier <chemillier.julien@gmail.com>
2022-03-25 19:19:57 +0100

    Add compile flags

--- make.sh
+++ make.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+FLAGS="-std=c99 -lm"
+
 if [[ $1 == "preview" ]]; then
 	[[ $2 ]] || set -- "$1" "build"
 	if [[ $2 == "build" ]]; then
 		mkdir -p out
-		gcc src/mnist/preview.c -o out/preview_mnist
+		gcc src/mnist/preview.c -o out/preview_mnist $FLAGS
 		exit
 	elif [[ $2 == "train" ]]; then
 		[[ -f out/preview_mnist ]] || $0 preview build
