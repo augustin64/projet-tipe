4a8ab009ce9997c36d7f973c9f5f785482673efd
augustin64 <augustin.lucas64@gmail.com>
2022-03-27 15:03:13 +0200

    Add make.sh instructions

--- make.sh
+++ make.sh
@@ -6,7 +6,9 @@ if [[ $1 == "preview" ]]; then
 	[[ $2 ]] || set -- "$1" "build"
 	if [[ $2 == "build" ]]; then
 		mkdir -p out
+		echo "Compilation de src/mnist/preview.c"
 		gcc src/mnist/preview.c -o out/preview_mnist $FLAGS
+		echo "Fait."
 		exit
 	elif [[ $2 == "train" ]]; then
 		[[ -f out/preview_mnist ]] || $0 preview build
@@ -24,7 +26,9 @@ if [[ $1 == "test" ]]; then
 	if [[ $2 == "build" ]]; then
 		mkdir -p out
 		for i in $(ls test); do
+			echo "Compilation de test/$i"
 			gcc "test/$i" -o "out/test_$(echo $i | awk -F. '{print $1}')" $FLAGS
+			echo "Fait."
 		done
 		exit
 	elif [[ $2 == "run" ]]; then
@@ -37,3 +41,8 @@ if [[ $1 == "test" ]]; then
 		exit
 	fi
 fi
+
+echo "Usage:"
+echo -e "\t$0 preview ( build | train | t10k )"
+echo -e "\t$0 test    ( build | run )\n"
+echo -e "Les fichiers de test sont recompilés à chaque exécution,\nles autres programmes sont compilés automatiquement si manquants"
