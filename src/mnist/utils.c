af02b87345be22a84979dd3781d4905ec5e14391
augustin64 <augustin.lucas64@gmail.com>
2022-04-19 16:26:28 +0200

    Update utils.c

--- src/mnist/utils.c
+++ src/mnist/utils.c
@@ -9,7 +9,7 @@
 void print_biais(char* filename) {
     Reseau* reseau = lire_reseau(".cache/reseau.bin");
 
-    for (int i=0; i < reseau->nb_couches; i++) {
+    for (int i=1; i < reseau->nb_couches -1; i++) {
         printf("Couche %d\n", i);
         for (int j=0; j < reseau->couches[i]->nb_neurones; j++) {
             printf("Couche %d\tNeurone %d\tBiais: %0.1f\n", i, j, reseau->couches[i]->neurones[j]->biais);
@@ -17,7 +17,7 @@ void print_biais(char* filename) {
     }
 }
 
-int main(int argc, int* argv) {
+int main(int argc, char* argv[]) {
     print_biais(".cache/reseau.bin");
     return 1;
 }
