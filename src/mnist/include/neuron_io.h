4afe37635355c376d167620d28fde441b4c78a27
Julien Chemillier <chemillier.julien@gmail.com>
2022-06-30 10:26:17 +0200

    Update comments of neuron_io

--- src/mnist/include/neuron_io.h
+++ src/mnist/include/neuron_io.h
@@ -8,18 +8,75 @@
 #ifndef DEF_NEURON_IO_H
 #define DEF_NEURON_IO_H
 
+
+
+// Lecture d'un réseau neuronal 
+
+/* 
+* Lit un neurone
+*/
 Neuron* read_neuron(uint32_t nb_weights, FILE *ptr);
+
+/*
+* Lit une couche de neurones
+*/
 Neuron** read_neurons(uint32_t nb_neurons, uint32_t nb_weights, FILE *ptr);
+
+/*
+* Charge l'entièreté du réseau neuronal depuis un fichier binaire
+*/
 Network* read_network(char* filename);
 
+
+
+
+// Écriture d'un réseau neuronal
+
+/*
+* Écrit un neurone dans le fichier pointé par *ptr
+*/
 void write_neuron(Neuron* neuron, int weights, FILE *ptr);
+
+/*
+* Stocke l'entièreté du réseau neuronal dans un fichier binaire
+*/
 void write_network(char* filename, Network* network);
 
+
+
+
+// Lecture des calculs de la backpropagation d'un réseau neuronal
+
+/*
+* Lit un neurone
+*/
 Neuron* read_delta_neuron(uint32_t nb_weights, FILE *ptr);
+
+/*
+* Lit une couche de neurones
+*/
 Neuron** read_delta_neurons(uint32_t nb_neurons, uint32_t nb_weights, FILE *ptr);
+
+/*
+* Charge l'entièreté du réseau neuronal depuis un fichier binaire
+*/
 Network* read_delta_network(char* filename);
 
+
+
+
+// Écriture des calculs de la backpropagation d'un réseau neuronal
+
+/*
+* Écrit les calculs de backpropagation effectués sur
+* un neurone dans le fichier pointé par *ptr
+*/
 void write_delta_neuron(Neuron* neuron, int weights, FILE *ptr);
+
+/*
+* Enregistre les calculs de backpropagation effectués
+* sur un réseau dans un fichier
+*/
 void write_delta_network(char* filename, Network* network);
 
 #endif
--- src/mnist/neuron_io.c
+++ src/mnist/neuron_io.c
@@ -10,9 +10,6 @@
 #define DELTA_MAGIC_NUMBER 2024
 
 
-/* Lecture d'un réseau neuronal */
-
-
 Neuron* read_neuron(uint32_t nb_weights, FILE *ptr) {
     Neuron* neuron = (Neuron*)malloc(sizeof(Neuron));
     float activation;
@@ -43,7 +40,6 @@ Neuron* read_neuron(uint32_t nb_weights, FILE *ptr) {
 }
 
 
-// Lit une couche de neurones
 Neuron** read_neurons(uint32_t nb_neurons, uint32_t nb_weights, FILE *ptr) {
     Neuron** neurons = (Neuron**)malloc(sizeof(Neuron*)*nb_neurons);
     for (int i=0; i < (int)nb_neurons; i++) {
@@ -53,7 +49,6 @@ Neuron** read_neurons(uint32_t nb_neurons, uint32_t nb_weights, FILE *ptr) {
 }
 
 
-// Charge l'entièreté du réseau neuronal depuis un fichier binaire
 Network* read_network(char* filename) {
     FILE *ptr;
     Network* network = (Network*)malloc(sizeof(Network));
@@ -96,10 +91,6 @@ Network* read_network(char* filename) {
 }
 
 
-/* Écriture d'un réseau neuronal */
-
-
-// Écrit un neurone dans le fichier pointé par *ptr
 void write_neuron(Neuron* neuron, int weights, FILE *ptr) {
     float buffer[weights+2];
 
@@ -112,7 +103,6 @@ void write_neuron(Neuron* neuron, int weights, FILE *ptr) {
 }
 
 
-// Stocke l'entièreté du réseau neuronal dans un fichier binaire
 void write_network(char* filename, Network* network) {
     FILE *ptr;
     int nb_layers = network->nb_layers;
@@ -138,9 +128,6 @@ void write_network(char* filename, Network* network) {
 
     fclose(ptr);
 }
-
-
-/* Lecture des calculs de la backpropagation d'un réseau neuronal */
 
 
 Neuron* read_delta_neuron(uint32_t nb_weights, FILE *ptr) {
@@ -172,7 +159,6 @@ Neuron* read_delta_neuron(uint32_t nb_weights, FILE *ptr) {
 }
 
 
-// Lit une couche de neurones
 Neuron** read_delta_neurons(uint32_t nb_neurons, uint32_t nb_weights, FILE *ptr) {
     Neuron** neurons = (Neuron**)malloc(sizeof(Neuron*)*nb_neurons);
     for (int i=0; i < (int)nb_neurons; i++) {
@@ -182,7 +168,6 @@ Neuron** read_delta_neurons(uint32_t nb_neurons, uint32_t nb_weights, FILE *ptr)
 }
 
 
-// Charge l'entièreté du réseau neuronal depuis un fichier binaire
 Network* read_delta_network(char* filename) {
     FILE *ptr;
     Network* network = (Network*)malloc(sizeof(Network));
@@ -225,11 +210,6 @@ Network* read_delta_network(char* filename) {
 }
 
 
-/* Écriture des calculs de la backpropagation d'un réseau neuronal */
-
-
-// Écrit les calculs de backpropagation effectués sur
-// un neurone dans le fichier pointé par *ptr
 void write_delta_neuron(Neuron* neuron, int weights, FILE *ptr) {
     float buffer[weights+2];
 
@@ -242,7 +222,6 @@ void write_delta_neuron(Neuron* neuron, int weights, FILE *ptr) {
 }
 
 
-// Enregistre les calculs de backpropagation effectués sur un réseau dans un fichier
 void write_delta_network(char* filename, Network* network) {
     FILE *ptr;
     int nb_layers = network->nb_layers;
