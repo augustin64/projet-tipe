f468883eb13909014b1698c3961391cc553ca194
augustin64 <me.git@augustin64.fr>
2023-01-17 15:08:47 +0100

    Update scripts

--- src/scripts/visualisation.py
+++ src/scripts/visualisation.py
@@ -28,7 +28,7 @@ def image_from_file(filepath, dest="./images/"):
         png.from_array(data[i], 'L').save(os.path.join(dest, f"{i}.png"))
 
 
-def image_from_list(filepath, dest="data.png", exp=False):
+def image_from_list(filepath, exp=False):
     """
     Enregistre une liste de poids sous forme d'une image
     exp sert à spécifier si il faut passer à une forme exponentielle
@@ -54,13 +54,13 @@ def image_from_list(filepath, dest="data.png", exp=False):
         for j in range(IMAGE_HEIGHT):
             new_data[i][j] = data[i*IMAGE_HEIGHT+j]
 
-    png.from_array(new_data, 'L').save(dest)
+    return new_data
 
 
 def graph_from_test_reseau(erreurs, reussites):
     """
     Affiche un graphique à partir d'un fichier contenant les
-    réussites et d'un autre contenant les erreurs (sortie brutes de out/main)
+    réussites et d'un autre contenant les erreurs (sortie brutes de out/mnist_main)
     """
     with open(erreurs, "r", encoding="utf8") as f:
         data = f.read()
@@ -113,7 +113,21 @@ def images_neurons(neurons, dest="neurons", exp=False):
     Afin de créer un ensemble d'image visualisant les poids
     """
     os.makedirs(dest, exist_ok=True)
+    data = []
     for i in neurons:
         os.system(f"./make.sh utils print-poids-neurone --reseau \
             .cache/reseau.bin --neurone {i} > .cache/poids.txt")
-        image_from_list(".cache/poids.txt", os.path.join(dest, f"{i}.png"), exp=exp)
+        data.append(image_from_list(".cache/poids.txt", exp=exp))
+
+    new_data = data.copy()
+
+    for i, _ in enumerate(new_data):
+        for j, _ in enumerate(new_data[i]):
+            for k, _ in enumerate(new_data[i][j]):
+                for l, _ in enumerate(new_data):
+                    if i != l:
+                        new_data[i][j][k] -= data[l][j][k] * 0.11
+                new_data[i][j][k] = max(int(new_data[i][j][k]), 0)
+    
+    for i in neurons:
+        png.from_array(data[i], 'L').save(os.path.join(dest, f"{i}.png"))
