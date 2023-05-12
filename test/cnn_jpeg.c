9474734a8f045df3b4e19aa4b8717c38ec88308b
augustin64 <me.git@augustin64.fr>
2023-05-12 16:18:03 +0200

    Update jpeg test

--- test/cnn_jpeg.c
+++ test/cnn_jpeg.c
@@ -1,13 +1,16 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <omp.h>
 
+
+#include "../src/common/include/colors.h"
+#include "../src/common/include/utils.h"
 #include "../src/cnn/include/jpeg.h"
-#include "../src/include/colors.h"
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         printf("Pas de dataset en argument, test avorté\n");
-        // On n'arrête pas le processus avce un code de sortie
+        // On arrête le processus avec un code de sortie 0
         // pour pouvoir utiliser `make run-tests` dans des scripts
         // sans avoir à spécifier d'arguments supplémentaires
         return 0;
@@ -22,6 +25,22 @@ int main(int argc, char* argv[]) {
         return 1;
     }
     #endif
+
+    // Calcul du temps de chargement des images une à une
+    double start_time, end_time;
+
+    int N = min(100000, dataset->numImages);
+    start_time = omp_get_wtime();
+    printf("Chargement de %d images\n", N);
+    for (int i=0; i < N; i++) {
+        imgRawImage* image = loadJpegImageFile(dataset->fileNames[i]);
+        free(image->lpData);
+        free(image);
+    }
+    printf("OK\n");
+    end_time = omp_get_wtime();
+    printf("Temps par image (calculé sur une moyenne de %d): %lf s\n", N, (end_time - start_time)/N);
+
     for (int i=0; i < (int)dataset->numImages; i++) {
         if (!dataset->fileNames[i]) {
             printf_error("Nom de fichier non chargé à l'index ");
